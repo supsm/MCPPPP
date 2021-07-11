b1ddb0bab18c6cf70ddba3154140dedf71d0b6bd
supsm <supsm2017@gmail.com>
2021-07-11 15:30:10 -0700

    change to clang, gui improvements

--- mcpppp.cxx
+++ mcpppp.cxx
@@ -2,38 +2,95 @@
 
 #include "mcpppp.h"
 
+void UI::cb_log_i(Fl_Input* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_log(Fl_Input* o, void* v) {
+  ((UI*)(o->parent()->user_data()))->cb_log_i(o,v);
+}
+
+void UI::cb_timestamptrue_i(Fl_Button* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_timestamptrue(Fl_Button* o, void* v) {
+  ((UI*)(o->parent()->parent()->user_data()))->cb_timestamptrue_i(o,v);
+}
+
+void UI::cb_timestampfalse_i(Fl_Button* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_timestampfalse(Fl_Button* o, void* v) {
+  ((UI*)(o->parent()->parent()->user_data()))->cb_timestampfalse_i(o,v);
+}
+
+void UI::cb_outputlevel_i(Fl_Counter* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_outputlevel(Fl_Counter* o, void* v) {
+  ((UI*)(o->parent()->user_data()))->cb_outputlevel_i(o,v);
+}
+
+void UI::cb_loglevel_i(Fl_Counter* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_loglevel(Fl_Counter* o, void* v) {
+  ((UI*)(o->parent()->user_data()))->cb_loglevel_i(o,v);
+}
+
+void UI::cb_deletesourcetrue_i(Fl_Button* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_deletesourcetrue(Fl_Button* o, void* v) {
+  ((UI*)(o->parent()->parent()->user_data()))->cb_deletesourcetrue_i(o,v);
+}
+
+void UI::cb_deletesourcefalse_i(Fl_Button* o, void* v) {
+  settingchanged(o, v);
+}
+void UI::cb_deletesourcefalse(Fl_Button* o, void* v) {
+  ((UI*)(o->parent()->parent()->user_data()))->cb_deletesourcefalse_i(o,v);
+}
+
 UI::UI() {
-  { window = new Fl_Window(660, 390, "MCPPPP");
+  { window = new Fl_Window(660, 411, "MCPPPP");
+    window->color((Fl_Color)48);
     window->user_data((void*)(this));
-    { Fl_Button* o = new Fl_Button(525, 350, 110, 30, "Run");
-      o->down_box(FL_UP_BOX);
+    { Fl_Button* o = new Fl_Button(525, 365, 110, 30, "Run");
+      o->tooltip("Start conversion process");
+      o->box(FL_BORDER_BOX);
+      o->down_box(FL_BORDER_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
       o->callback((Fl_Callback*)run);
     } // Fl_Button* o
     { // Output in place of stdout
-      text_display = new Fl_Text_Display(25, 25, 395, 275);
+      text_display = new Fl_Text_Display(25, 40, 395, 275);
       text_display->box(FL_BORDER_BOX);
     } // Fl_Text_Display* text_display
-    { Fl_Check_Button* o = new Fl_Check_Button(25, 360, 56, 16, "FSB");
+    { Fl_Check_Button* o = new Fl_Check_Button(25, 375, 56, 16, "FSB");
+      o->tooltip("FabricSkyboxes");
       o->down_box(FL_DOWN_BOX);
       o->value(1);
       o->callback((Fl_Callback*)conversion);
       o->when(FL_WHEN_CHANGED);
     } // Fl_Check_Button* o
-    { Fl_Check_Button* o = new Fl_Check_Button(185, 360, 56, 16, "VMT");
+    { Fl_Check_Button* o = new Fl_Check_Button(185, 375, 56, 16, "VMT");
+      o->tooltip("Varied Mob Textures");
       o->down_box(FL_DOWN_BOX);
       o->value(1);
       o->callback((Fl_Callback*)conversion);
       o->when(FL_WHEN_CHANGED);
     } // Fl_Check_Button* o
-    { Fl_Check_Button* o = new Fl_Check_Button(360, 360, 56, 16, "CIM");
+    { Fl_Check_Button* o = new Fl_Check_Button(360, 375, 56, 16, "CIM");
+      o->tooltip("Chime");
       o->down_box(FL_DOWN_BOX);
       o->value(1);
       o->callback((Fl_Callback*)conversion);
       o->when(FL_WHEN_CHANGED);
     } // Fl_Check_Button* o
-    { scroll = new Fl_Scroll(445, 40, 190, 260, "Resourcepacks to convert");
+    { scroll = new Fl_Scroll(445, 55, 190, 260, "Resourcepacks to convert");
       { // padding
-        Fl_Check_Button* o = new Fl_Check_Button(445, 45, 190, 15);
+        Fl_Check_Button* o = new Fl_Check_Button(445, 60, 190, 15);
         o->down_box(FL_DOWN_BOX);
         o->labeltype(FL_NO_LABEL);
         o->hide();
@@ -41,24 +98,70 @@ UI::UI() {
       } // Fl_Check_Button* o
       scroll->end();
     } // Fl_Scroll* scroll
-    { Fl_Button* o = new Fl_Button(430, 315, 64, 25, "Browse");
+    { Fl_Button* o = new Fl_Button(430, 330, 64, 25, "Browse");
+      o->tooltip("Browse for resourcepacks folders");
+      o->box(FL_BORDER_BOX);
+      o->down_box(FL_BORDER_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
       o->callback((Fl_Callback*)browse);
     } // Fl_Button* o
-    { Fl_Button* o = new Fl_Button(525, 315, 110, 25, "Reload");
+    { Fl_Button* o = new Fl_Button(525, 330, 110, 25, "Reload");
+      o->tooltip("Reload list of resourcepacks");
+      o->box(FL_BORDER_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
       o->callback((Fl_Callback*)reload);
     } // Fl_Button* o
-    { path_input = new Fl_Input(130, 315, 290, 24, "Resourcepacks:");
+    { path_input = new Fl_Input(130, 330, 290, 24, "Resourcepacks:");
+      path_input->tooltip("Input resourcepacks folder manually");
+      path_input->box(FL_BORDER_BOX);
       path_input->callback((Fl_Callback*)editpath);
-      path_input->when(FL_WHEN_ENTER_KEY);
+      path_input->when(FL_WHEN_CHANGED);
     } // Fl_Input* path_input
+    { Fl_Button* o = new Fl_Button(25, 10, 65, 20, "Settings");
+      o->tooltip("Edit Settings");
+      o->box(FL_FLAT_BOX);
+      o->down_box(FL_FLAT_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
+      o->callback((Fl_Callback*)opensettings);
+    } // Fl_Button* o
+    { Fl_Button* o = new Fl_Button(570, 10, 65, 20, "Help");
+      o->tooltip("Open help screen");
+      o->box(FL_FLAT_BOX);
+      o->down_box(FL_FLAT_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
+      o->callback((Fl_Callback*)openhelp);
+    } // Fl_Button* o
+    { box1 = new Fl_Box(25, 10, 65, 20);
+      box1->box(FL_BORDER_FRAME);
+      box1->color((Fl_Color)34);
+    } // Fl_Box* box1
+    { box2 = new Fl_Box(570, 10, 65, 20);
+      box2->box(FL_BORDER_FRAME);
+      box2->color((Fl_Color)34);
+    } // Fl_Box* box2
     window->end();
   } // Fl_Window* window
   { edit_paths = new Fl_Window(270, 300, "Edit Paths");
+    edit_paths->color((Fl_Color)48);
     edit_paths->user_data((void*)(this));
     { Fl_Button* o = new Fl_Button(10, 270, 120, 20, "Add");
+      o->tooltip("Browse for resourcepacks folder");
+      o->box(FL_BORDER_BOX);
+      o->down_box(FL_BORDER_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
       o->callback((Fl_Callback*)addrespath);
     } // Fl_Button* o
     { Fl_Button* o = new Fl_Button(140, 270, 120, 20, "Delete");
+      o->tooltip("Remove selected folder");
+      o->box(FL_BORDER_BOX);
+      o->down_box(FL_BORDER_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
       o->callback((Fl_Callback*)deleterespath);
     } // Fl_Button* o
     { paths = new Fl_Scroll(10, 15, 250, 245);
@@ -69,6 +172,117 @@ UI::UI() {
     } // Fl_Scroll* paths
     edit_paths->end();
   } // Fl_Window* edit_paths
+  { settings = new Fl_Window(300, 240, "Settings");
+    settings->user_data((void*)(this));
+    { Fl_Box* o = new Fl_Box(20, 15, 100, 20, "log");
+      o->tooltip("A log file where logs will be stored");
+      o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
+    } // Fl_Box* o
+    { Fl_Box* o = new Fl_Box(20, 45, 100, 20, "timestamp");
+      o->tooltip("Whether to add timestamp to console");
+      o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
+    } // Fl_Box* o
+    { Fl_Box* o = new Fl_Box(20, 75, 100, 20, "outputLevel");
+      o->tooltip("How much info should be outputted \\n1 Spam\\n2 Info\\n3 Important\\n4 Warnin\
+g\\n5 Error");
+      o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
+    } // Fl_Box* o
+    { Fl_Box* o = new Fl_Box(20, 105, 100, 20, "logLevel");
+      o->tooltip("Same as outputLevel, but for logs");
+      o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
+    } // Fl_Box* o
+    { Fl_Box* o = new Fl_Box(20, 135, 100, 20, "deleteSource");
+      o->tooltip("Delete source (of/mcp) files when done converting. Cannot re-convert");
+      o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
+    } // Fl_Box* o
+    { log = new Fl_Input(130, 15, 150, 20);
+      log->box(FL_BORDER_BOX);
+      log->labeltype(FL_NO_LABEL);
+      log->callback((Fl_Callback*)cb_log);
+      log->when(FL_WHEN_CHANGED);
+    } // Fl_Input* log
+    { Fl_Group* o = new Fl_Group(130, 45, 150, 20);
+      { timestamptrue = new Fl_Button(130, 45, 75, 20, "True");
+        timestamptrue->type(102);
+        timestamptrue->box(FL_FLAT_BOX);
+        timestamptrue->down_box(FL_BORDER_BOX);
+        timestamptrue->value(1);
+        timestamptrue->color(FL_DARK2);
+        timestamptrue->selection_color((Fl_Color)43);
+        timestamptrue->callback((Fl_Callback*)cb_timestamptrue);
+      } // Fl_Button* timestamptrue
+      { timestampfalse = new Fl_Button(205, 45, 75, 20, "False");
+        timestampfalse->type(102);
+        timestampfalse->box(FL_FLAT_BOX);
+        timestampfalse->down_box(FL_BORDER_BOX);
+        timestampfalse->color(FL_DARK2);
+        timestampfalse->selection_color((Fl_Color)43);
+        timestampfalse->callback((Fl_Callback*)cb_timestampfalse);
+      } // Fl_Button* timestampfalse
+      o->end();
+    } // Fl_Group* o
+    { outputlevel = new Fl_Counter(130, 75, 150, 20);
+      outputlevel->box(FL_BORDER_BOX);
+      outputlevel->labeltype(FL_NO_LABEL);
+      outputlevel->minimum(1);
+      outputlevel->maximum(5);
+      outputlevel->step(1);
+      outputlevel->value(3);
+      outputlevel->callback((Fl_Callback*)cb_outputlevel);
+    } // Fl_Counter* outputlevel
+    { loglevel = new Fl_Counter(130, 105, 150, 20);
+      loglevel->box(FL_BORDER_BOX);
+      loglevel->labeltype(FL_NO_LABEL);
+      loglevel->minimum(1);
+      loglevel->maximum(5);
+      loglevel->step(1);
+      loglevel->value(2);
+      loglevel->callback((Fl_Callback*)cb_loglevel);
+    } // Fl_Counter* loglevel
+    { Fl_Group* o = new Fl_Group(130, 135, 150, 20);
+      { deletesourcetrue = new Fl_Button(130, 135, 75, 20, "True");
+        deletesourcetrue->type(102);
+        deletesourcetrue->box(FL_FLAT_BOX);
+        deletesourcetrue->down_box(FL_BORDER_BOX);
+        deletesourcetrue->color(FL_DARK2);
+        deletesourcetrue->selection_color((Fl_Color)43);
+        deletesourcetrue->callback((Fl_Callback*)cb_deletesourcetrue);
+      } // Fl_Button* deletesourcetrue
+      { deletesourcefalse = new Fl_Button(205, 135, 75, 20, "False");
+        deletesourcefalse->type(102);
+        deletesourcefalse->box(FL_FLAT_BOX);
+        deletesourcefalse->down_box(FL_BORDER_BOX);
+        deletesourcefalse->value(1);
+        deletesourcefalse->color(FL_DARK2);
+        deletesourcefalse->selection_color((Fl_Color)43);
+        deletesourcefalse->callback((Fl_Callback*)cb_deletesourcefalse);
+      } // Fl_Button* deletesourcefalse
+      o->end();
+    } // Fl_Group* o
+    { Fl_Button* o = new Fl_Button(20, 185, 260, 25, "Save");
+      o->tooltip("Save settings");
+      o->box(FL_BORDER_BOX);
+      o->down_box(FL_BORDER_BOX);
+      o->color(FL_DARK2);
+      o->selection_color((Fl_Color)43);
+      o->callback((Fl_Callback*)savesettings);
+    } // Fl_Button* o
+    { savewarning = new Fl_Box(20, 215, 260, 20, "Warning: unsaved changes");
+      savewarning->labelfont(1);
+      savewarning->labelcolor((Fl_Color)1);
+      savewarning->hide();
+    } // Fl_Box* savewarning
+    settings->end();
+  } // Fl_Window* settings
+  { help = new Fl_Window(400, 320, "Help");
+    help->user_data((void*)(this));
+    { Fl_Help_View* o = new Fl_Help_View(0, 0, 400, 320);
+      o->box(FL_FLAT_BOX);
+      o->value("<html><body><p>Hi there, welcome to MCPPPP.</p><p>To begin, click the Browse button to browse for a path. Click the Add button and locate your Minecraft resourceapcks folder.<br>At the bottom, select which conversions you want to do, and select which resourcepacks to convert at the right (you may need to reload first).<br>When you are done with that, you may press the Run button to start the conversion process.</p><p>For additional support, please check out <a href=\"https://github.com/supsm/MCPPPP/blob/master/GUI.md\">https://github.com/supsm/MCPPPP/blob/master/GUI.md</a></p></body></html>");
+      o->textsize(16);
+    } // Fl_Help_View* o
+    help->end();
+  } // Fl_Window* help
 }
 
 void UI::show() {
--- mcpppp.h
+++ mcpppp.h
@@ -14,8 +14,15 @@ extern void browse(Fl_Button*, void*);
 extern void reload(Fl_Button*, void*);
 #include <FL/Fl_Input.H>
 extern void editpath(Fl_Input*, void*);
+extern void opensettings(Fl_Button*, void*);
+extern void openhelp(Fl_Button*, void*);
+#include <FL/Fl_Box.H>
 extern void addrespath(Fl_Button*, void*);
 extern void deleterespath(Fl_Button*, void*);
+#include <FL/Fl_Group.H>
+#include <FL/Fl_Counter.H>
+extern void savesettings(Fl_Button*, void*);
+#include <FL/Fl_Help_View.H>
 
 class UI {
 public:
@@ -24,8 +31,49 @@ public:
   Fl_Text_Display *text_display;
   Fl_Scroll *scroll;
   Fl_Input *path_input;
+  Fl_Box *box1;
+  Fl_Box *box2;
   Fl_Window *edit_paths;
   Fl_Scroll *paths;
+  Fl_Window *settings;
+  Fl_Input *log;
+private:
+  inline void cb_log_i(Fl_Input*, void*);
+  static void cb_log(Fl_Input*, void*);
+public:
+  Fl_Button *timestamptrue;
+private:
+  inline void cb_timestamptrue_i(Fl_Button*, void*);
+  static void cb_timestamptrue(Fl_Button*, void*);
+public:
+  Fl_Button *timestampfalse;
+private:
+  inline void cb_timestampfalse_i(Fl_Button*, void*);
+  static void cb_timestampfalse(Fl_Button*, void*);
+public:
+  Fl_Counter *outputlevel;
+private:
+  inline void cb_outputlevel_i(Fl_Counter*, void*);
+  static void cb_outputlevel(Fl_Counter*, void*);
+public:
+  Fl_Counter *loglevel;
+private:
+  inline void cb_loglevel_i(Fl_Counter*, void*);
+  static void cb_loglevel(Fl_Counter*, void*);
+public:
+  Fl_Button *deletesourcetrue;
+private:
+  inline void cb_deletesourcetrue_i(Fl_Button*, void*);
+  static void cb_deletesourcetrue(Fl_Button*, void*);
+public:
+  Fl_Button *deletesourcefalse;
+private:
+  inline void cb_deletesourcefalse_i(Fl_Button*, void*);
+  static void cb_deletesourcefalse(Fl_Button*, void*);
+public:
+  Fl_Box *savewarning;
+  Fl_Window *help;
   void show();
 };
+extern void settingchanged(Fl_Widget*, void*);
 #endif
