246509912edbbb731b5c82c7586305061b506a26
supsm <supsm2017@gmail.com>
2021-12-15 13:09:18 -0800

    license stuffs

--- utility.h
+++ utility.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-constexpr auto VERSION = "0.5.7"; // MCPPPP version
+constexpr auto VERSION = "0.5.8"; // MCPPPP version
 constexpr int PACK_VER = 8; // pack.mcmeta pack format
 
 #include <algorithm>
