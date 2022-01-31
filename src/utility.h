4781ff9c64b37914cd15f9c15275c3926f273585
supsm <supsm2017@gmail.com>
2022-01-30 20:19:54 -0800

    vmt png only (no prop yet)

--- src/utility.h
+++ src/utility.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-constexpr auto VERSION = "0.6.1"; // MCPPPP version
+constexpr auto VERSION = "0.7.0"; // MCPPPP version
 constexpr int PACK_VER = 8; // pack.mcmeta pack format
 
 #ifdef _WIN32
