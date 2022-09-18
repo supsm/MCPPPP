ff2faee1da8a644d66722c5743fd45e07c1fced8
supsm <supsm2017@gmail.com>
2022-09-18 11:57:11 -0700

    fix crash when loading libraries

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MCPPPP Mod
 Mod version of MCPPPP. It will extract and run the MCPPPP binaries when you start your game. It may take some time. MCPPPP logs will be visible in mc console.  
 To get the mod, download it from the latest release. You will want the file without -dev or -sources.  
+
+**Important**: Remember to have [git lfs](https://git-lfs.github.com/) when cloning  
--- src/main/java/supsm/mcpppp/mcpppp.java
+++ src/main/java/supsm/mcpppp/mcpppp.java
@@ -1,19 +1,19 @@
 package supsm.mcpppp;
 
-import net.fabricmc.api.ModInitializer;
-
 import java.io.File;
 import java.io.InputStream;
-import org.apache.commons.io.FileUtils;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.fabricmc.api.ModInitializer;
+
 class jni
 {
-	private static void extract(String str)
+	// extract a file from .jar resources to path
+	private static void extract(String str, String path)
 	{
-		String path = System.getProperty("java.library.path");
 		File file = new File(path + "/" + str);
 		InputStream stream = jni.class.getResourceAsStream("/" + str);
 		try
@@ -25,20 +25,41 @@ class jni
 			e.printStackTrace();
 		}
 	}
+
 	static
 	{
-		extract("mcpppp.dll");
-		extract("libmcpppp.so");
-		extract("libmcpppp.dylib");
-		System.loadLibrary("mcpppp");
+		String path = System.getProperty("user.dir") + "/" + mcpppp.lib_dir;
+		int libs_loaded = 0;
+		for (String s : mcpppp.jni_libs)
+		{
+			// try to load each library until it works
+			extract(s, path);
+			try
+			{
+				System.load(path + "/" + s);
+				libs_loaded++;
+			}
+			catch (UnsatisfiedLinkError e)
+			{
+				mcpppp.LOGGER.info(e.getMessage());
+			}
+		}
+		if (libs_loaded == 0)
+		{
+			mcpppp.LOGGER.error("No valid MCPPPP JNI library found");
+		}
 	}
+
 	public native void run(String path, String os);
 }
 
 public class mcpppp implements ModInitializer
 {
 	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String[] jni_libs = { "mcpppp.dll", "libmcpppp.so", "libmcpppp.dylib" };
+	public static final String lib_dir = "mcpppp-libs";
 
+	// delete file
 	private static void delete(String str)
 	{
 		try
@@ -52,6 +73,7 @@ public class mcpppp implements ModInitializer
 		}
 	}
 
+	// extract major os version from version string
 	private static int getmajorversion(String version)
     {
         String nums = new String();
@@ -72,20 +94,34 @@ public class mcpppp implements ModInitializer
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		// name of os
 		String OS = System.getProperty("os.name");
-		String path = System.getProperty("user.dir") + "/resourcepacks";
+		// minecraft directory
+		String path = System.getProperty("user.dir");
+		// version of os
 		String version = System.getProperty("os.version");
 
-
+		// jni binaries only work on MacOS 12+
 		if (OS.toLowerCase().contains("mac") && getmajorversion(version) < 11)
 		{
 			LOGGER.error("Mac OS outdated, not running MCPPPP");
 			return;
 		}
 
-		new jni().run(path, OS);
-		delete("mcpppp.dll");
-		delete("libmcpppp.so");
-		delete("libmcpppp.dylib");
+		try
+		{
+			new jni().run(path + "/resourcepacks", OS + " " + version);
+		}
+		catch (UnsatisfiedLinkError e)
+		{
+			LOGGER.error("MCPPPP JNI library improperly loaded, conversion skipped");
+		}
+
+		for (String s : jni_libs)
+		{
+			delete(path + "/" + lib_dir + "/" + s);
+		}
+		delete(path + "/" + lib_dir);
 	}
 }
